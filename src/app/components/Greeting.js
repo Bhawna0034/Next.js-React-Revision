bc43d59c95caad4f2e9f6ecb2882f5a4192ef81a
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-08 11:47:57 +0530

    Jsx, Functional components, Props

---
+++ src/app/components/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greeting = ({name}) => {
+  return (
+    <div className='text-5xl font-bold flex items-center justify-center h-lvh'>
+      Welcome {name}, to React World!
+    </div>
+  )
+}
+
+export default Greeting

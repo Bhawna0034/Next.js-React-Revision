eb8edb4881ef94e17263a881568d57370ff053ff
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-08 23:20:02 +0530

    Handling Loading and Error states

--- src/app/components/Fetch API Data/FetchData.js
+++ src/app/components/Fetch API Data/FetchData.js
@@ -2,34 +2,50 @@
 import React, { useEffect, useState } from "react";
 
 const FetchData = () => {
-  const [apiData, setApiData] = useState([]);
+  const [apiData, setApiData] = useState([]); // to store the fetched data
+  const [loading, setLoading] = useState(true); // to show loading while data is being fetched
+  const [error, setError] = useState(null); // to store any error message
+
   const API_URL = "https://jsonplaceholder.typicode.com/users";
   useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
-      .then((apiData) => setApiData(apiData))
-      .catch((error) => console.error("Error fetching data: ", error));
+      .then((apiData) => {
+        setApiData(apiData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error.message);
+    });
   }, []);
-  return <div>
-    <table className="w-full text-center border-collapse mb-4">
+
+  if(loading) return <p> Loading...</p>
+  if(error) return <p>{error.message}</p>
+  return (
+    <div>
+      <table className="w-full text-center border-collapse mb-4">
         <thead>
-            <tr className="bg-gray-100">
-                <th className="border px-4 py-2">UserId</th>
-                <th className="border px-4 py-2">Name</th>
-                <th className="border px-4 py-2">Email</th>
-            </tr>
+          <tr className="bg-gray-100">
+            <th className="border px-4 py-2">UserId</th>
+            <th className="border px-4 py-2">Name</th>
+            <th className="border px-4 py-2">Email</th>
+          </tr>
         </thead>
         <tbody>
-            {apiData.map((data) => {
-                return <tr key={data.id} className="border-b">
-                    <td className="border px-4 py-2">{data.id}</td>
-                    <td className="border px-4 py-2">{data.name}</td>
-                    <td className="border px-4 py-2">{data.email}</td>
-                </tr>
-            })}
+          {apiData.map((data) => {
+            return (
+              <tr key={data.id} className="border-b">
+                <td className="border px-4 py-2">{data.id}</td>
+                <td className="border px-4 py-2">{data.name}</td>
+                <td className="border px-4 py-2">{data.email}</td>
+              </tr>
+            );
+          })}
         </tbody>
-    </table>
-  </div>;
+      </table>
+    </div>
+  );
 };
 
 export default FetchData;

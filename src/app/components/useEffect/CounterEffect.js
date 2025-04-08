62b97da59ffc3cc6cdaed647066431d964c65425
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-08 22:54:54 +0530

    useEffect() Hook

---
+++ src/app/components/useEffect/CounterEffect.js
@@ -0,0 +1,19 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+const CounterEffect = () => {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.info("Count: ", count);
+
+    },[count]);
+  return (
+    <div className='flex flex-col gap-4 items-center'>
+        <h2 className='text-4xl font-bold'>{count}</h2>
+        <button onClick={() => setCount(count + 1)} className='bg-black text-white px-4 py-2 '>Increment</button>
+      
+    </div>
+  )
+}
+
+export default CounterEffect
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import ControlledInput from "./components/ControlledInput";
-import UnControlledInput from "./components/UnControlledInput";
+import CounterEffect from "./components/useEffect/CounterEffect";
 
 export default function Home() {
   const isLoggedIn = true;
   return (
     <main className="flex items-center justify-center h-lvh">
-     <UnControlledInput />
+     <CounterEffect />
     </main>
   );
 }

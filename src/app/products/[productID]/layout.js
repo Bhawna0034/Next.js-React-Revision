66b58e4dc8b985ef2a03c1eb4d17c67bed0e4e5d
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-11 09:45:54 +0530

    Nested Layout

---
+++ src/app/products/[productID]/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductLayout = ({children}) => {
+  return (
+    <div>
+      {children}
+      <h2>Featured Product Layout</h2>
+    </div>
+  )
+}
+
+export default ProductLayout
---
+++ src/app/products/[productID]/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductDetails = async({params}) => {
+    const {productID} = await params;
+    
+  return (
+    <div>
+      Product {productID} Details
+    </div>
+  )
+}
+
+export default ProductDetails

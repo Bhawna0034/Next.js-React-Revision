af67e76f76f843b1284f4d8cc6e9e33dace7b8b5
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-11 22:15:43 +0530

    Active Link Component

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,22 +1,62 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Product = () => {
-    const productID = 100;
+  const productID = 100;
+  const isActive = usePathname();
   return (
     <div>
       <h1 className="text-2xl font-bold">Product List</h1>
-      <h2 className="text-blue-800 underline font-semibold">
-        <Link href="/products/1">Product 1</Link>
+      <h2>
+        <Link
+          href="/products/1"
+          className={
+            isActive === "/products/1"
+              ? "text-blue-950 font-bold"
+              : "text-blue-800 underline"
+          }
+        >
+          Product 1
+        </Link>
       </h2>
-      <h2 className="text-blue-800 underline font-semibold">
-        <Link href="/products/2">Product 2</Link>
+      <h2>
+        <Link
+          href="/products/2"
+          className={
+            isActive === "/products/1"
+              ? "text-blue-950 font-bold"
+              : "text-blue-800 underline"
+          }
+        >
+          Product 2
+        </Link>
       </h2>
-      <h2 className="text-blue-800 underline font-semibold">
-        <Link href="/products/3" replace>Product 3</Link>
+      <h2>
+        <Link
+          href="/products/3"
+          className={
+            isActive === "/products/1"
+              ? "text-blue-950 font-bold"
+              : "text-blue-800 underline"
+          }
+          replace
+        >
+          Product 3
+        </Link>
       </h2>
-      <h2 className="text-blue-800 underline font-semibold">
-        <Link href={`/products/${productID}`}>Product {productID}</Link>
+      <h2>
+        <Link
+          href={`/products/${productID}`}
+          className={
+            isActive === "/products/1"
+              ? "text-blue-950 font-bold"
+              : "text-blue-800 underline"
+          }
+        >
+          Product {productID}
+        </Link>
       </h2>
     </div>
   );
